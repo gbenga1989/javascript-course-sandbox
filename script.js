2fe8a3daf4aa82d26a617861ba415543f12df2df
gbenga <odetoyinbog@gmail.com>
2025-03-24 12:13:02 +0100

    24-2

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let firstName = 'john';
+let lastName = 'doe';
+
+console.log(firstName, lastName);
+
+var age =30;
+
+console.log(age);
+
+
+// reasigning variables
+age = 32;
+console.log(age);
+
+let score;
+score = 1;
+console.clear(score);
+
+if (true){
+  score = score + 1
+}
+
+console.log(score);
