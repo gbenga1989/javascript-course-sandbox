f78faf230572560cf6d23e102abdada0da9b49b9
gbenga <odetoyinbog@gmail.com>
2025-03-24 13:06:11 +0100

    24-4

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
 
-let firstName = 'john';
-let lastName = 'doe';
+// let firstName = 'john';
+// let lastName = 'doe';
 
-console.log(firstName, lastName);
+// console.log(firstName, lastName);
 
-var age =30;
+// var age =30;
 
-console.log(age);
-
-
-// reasigning variables
-age = 32;
-console.log(age);
-
-let score;
-score = 1;
-console.clear(score);
-
-if (true){
-  score = score + 1
-}
+// console.log(age);
 
 
-console.log(score);
+// // reasigning variables
+// age = 32;
+// console.log(age);
 
-const x =100;
+// let score;
+// score = 1;
+// console.clear(score);
 
-const arr = [1,2,3,4];
-arr.push(5);
-console.log(arr);
+// if (true){
+//   score = score + 1
+// }
 
 
-const person = {
-  name: 'brad',
-};
+// console.log(score);
 
-person.name = 'john';
-person.mail = 'ghrnhs@gmail.com'
-console.log(person);
+// const x =100;
+
+// const arr = [1,2,3,4];
+// arr.push(5);
+// console.log(arr);
+
+
+// const person = {
+//   name: 'brad',
+// };
+
+// person.name = 'john';
+// person.mail = 'ghrnhs@gmail.com'
+// console.log(person);
+
+
+// // declare multiple variables
+
+// let a, b, c;
+
+// const d=10, e=20, f=30;
+// console.log(e);
+
+
+// // primitive data types
+// string
+const firstName = 'sam';
+
+const output = firstName;
+console.log(output, typeof output);
