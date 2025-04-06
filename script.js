06b70b2eff478947b079f1a2afa50df345f7833e
gbenga <odetoyinbog@gmail.com>
2025-04-06 11:06:16 +0100

    6-1

--- script.js
+++ script.js
@@ -399,12 +399,131 @@
 
   // arrays as params
   // the rest operator(...) turns everything that i have into an array
-  function getRandom(arr){
-    const randomIndex = Math.floor(Math.random() * arr.length);
+  // function getRandom(arr){
+  //   const randomIndex = Math.floor(Math.random() * arr.length);
 
-    const item = arr[randomIndex]
+  //   const item = arr[randomIndex]
 
-    console.log(item);
-  }
+  //   console.log(item);
+  // }
 
-  getRandom([1,2,3,4,5,6,7,8,9,10 ])
+  // getRandom([ ])
+
+
+  // 4.Global and fuunction scope- scope refers to the current area/context of an area of code
+
+  // alert('hello')
+  // const x = 100;
+  // console.log(x, 'in global');
+
+  // function run(){
+  //  console.log(window.innerHeight);
+  //  console.log(x, 'in function');
+  // }
+
+  // run();
+
+  // if(true){
+  //   console.log(x, 'in block');
+  // }
+
+  // function add(){
+  //   const y = 50;
+  //   console.log(y);
+  // }
+
+  // add();
+  // (local sope refers to the scope i am in)
+
+  // 5. Block scope
+  const x = 100; 
+
+// (this is a block)
+  // if(true){
+  //   const y = 200;
+  //   console.log(x + y);
+  // }
+
+  // // console.log( x + y);
+
+  // for(let i = 0; i <= 10; i++){
+  //   console.log(i);
+  // }
+
+  // // console.log(i);
+
+  // if (true){
+  //   const a = 500;
+  //   let b = 600;
+  //   var c = 700;
+  // }
+
+  // console.log(c);
+
+  // 6.Nested sequence
+  // function first(){
+
+  //   const x = 100;
+    
+  //   function second(){
+  //     const y = 200;
+  //     console.log( x + y);
+  //   }
+  //   second();
+  // }
+
+  // first();
+
+  // if (true){
+  //   const x = 100;
+
+  //   if (x === 100){
+  //     const y = 200;
+  //     console.log(x + y);
+  //   }
+  // }
+
+  // 7.Declare function
+  // function addDollarSign(value){
+  //   return '$' + value;
+  // }
+  // console.log(addDollarSign(100));
+  // // function expression
+  // const addPlusSign = function (value) {
+  //   return '+' + value;
+  // }
+
+  // console.log(addPlusSign(200));
+
+  // 8.Arrow functions
+  // function add(a,b){
+  //   return a + b;
+  // }
+
+  // arrow function
+  const add = (a,b) => {
+    return a + b;
+  };
+
+  // implicit return
+  const subtract = (a,b) => a-b;
+
+  const double = (a) => a * 2;
+  
+  // returning an object
+  const createObj = () => ({
+    name: 'Brad'
+  })
+
+  const numbers = [1];
+  numbers.forEach(function (n) {
+    console.log(n);
+  });
+
+  // arrow function in a call back
+  numbers.forEach((n) => console.log(n));
+
+  console.log(add(1,2));
+  console.log(subtract(10,2));
+  console.log(double(10));
+  console.log(createObj(10));
