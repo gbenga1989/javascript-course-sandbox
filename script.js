0eb60a41b307a7a7ede91b558c7147d562124102
gbenga <odetoyinbog@gmail.com>
2025-04-11 15:16:12 +0100

    11-4

--- script.js
+++ script.js
@@ -1242,7 +1242,7 @@ console.log(youngPeople);
 // challenge 2: Add all of the positive numbers in the array.
 
 const numbers = [2, -30, 50, 20, -12, -9, 7];
-const numbers2 = [1,2,3];
+const numbers2 = [1,2,3,4];
 
 const positiveSum = numbers
 .filter((number) => number > 0)
