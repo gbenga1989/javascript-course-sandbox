2da8ac662fc40154279726c28ce1419a5e543cae
gbenga <odetoyinbog@gmail.com>
2025-04-13 10:51:27 +0100

    13-5

--- script.js
+++ script.js
@@ -1280,4 +1280,4 @@ console.log(document.getElementById('main'));
 const main = document.getElementById('main');
 // main.innerHTML = '<h1> hello from main </h1>'
 
-document.querySelector('#main h1, 1,2,3,4 ').innerText = 'Hello';
+document.querySelector('#main h1, 1,2,3,4,5 ').innerText = 'Hello';
